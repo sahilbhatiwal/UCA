003a38e3ff2e3137b2c800c8d7c16c1caf185918
sahilbhatiwal <sahil0683.cse19@chirkara.edu.in>
2021-08-13 18:26:19 +0000

    added code for inversion count

--- inversion__count.c
+++ inversion__count.c
@@ -1,3 +1,38 @@
 /*
+ * Program to findInversion Count for an array indicates – how far (or close)
+ * the array is from being sorted. If the array is already
+ * sorted, then the inversion count is 0, but if the array is sorted in the reverse order, the inversion count is the maximum. 
+ *
+ * @Sahil Bhatiwal (1910990683), 13 - Aug - 2021
+ * Assignment 7 (Question 1)
  *
  */
+#include <stdio.h>
+
+int main()
+{
+    int len;
+    printf("Enter the length of array: ");
+    scanf("%d",&len);
+    int arr[n];
+    int count=0;
+    for(int i = 0; i < len; i++) {
+        scanf("%d",&arr[i]);
+    }
+    int j=0;
+    for(int i=1;i<len;i++)
+    {
+        int ele=arr[i];
+        for(j=i-1;j>=0;j--)
+        {
+            if(arr[j]>ele)
+            {
+                arr[j+1]=arr[j];
+                count++;
+            }
+        }
+        arr[j+1]=ele;
+    }
+   printf("%d",count);
+    return 0;
+}
